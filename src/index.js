8197b54dd80c1bd079137b04755cc4e7ead2fcf2
egrieshaber <eric.j.grieshaber@gmail.com>
2019-03-27 17:03:43 -0400

    added functionality and styling
    the game is now able to randomly select an author picture with list of
    books including some of which that do not belong to the author.  Also
    added styling to prohibit the author picture from overlapping the book
    titles.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
+
 
 const authors = [
   {
@@ -48,11 +50,22 @@ const authors = [
 ];
 
 function getTurnData(authors) {
-  const allBooks = authors.reduce(fuction (p, c, i) {
+  const allBooks = authors.reduce(function (p, c, i) {
     return p.concat(c.books);
   }, []);
-  
+  // the functions below use the shuffle and sample methods from the underscore
+  // library
+  const fourRandomBooks = shuffle(allBooks).slice(0,4);
+  const answer = sample(fourRandomBooks);
+
+  return {
+    books: fourRandomBooks,
+    author: authors.find((author) =>
+      author.books.some((title) =>
+        title === answer))
+  }
 }
+
 
 const state = {
   turnData: getTurnData(authors)
