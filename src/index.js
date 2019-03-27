2916ea0db4669faa722be99815f376cb30a40965
egrieshaber <eric.j.grieshaber@gmail.com>
2019-03-26 21:43:10 -0400

    added functionality
    added authors data to index.js, adding game functionality to
    AuthorQuiz.js, along with basic css adjustments

--- src/index.js
+++ src/index.js
@@ -4,7 +4,61 @@ import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+const authors = [
+  {
+    name: 'Mark Twain',
+    imageUrl: 'images/authors/marktwain.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: [
+      'The Adventures of Huckleberry Finn',
+      'Life On The Mississippi',
+      'Roughing It'
+    ]
+  },
+  {
+    name: 'Joseph Conrad',
+    imageUrl: 'images/authors/josephconrad.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['Heart of Darkness']
+  },
+  {
+    name: 'J.K. Rowling',
+    imageUrl: 'images/authors/jkrowling.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['Harry Potter and the Sorcerers Stone']
+  },
+  {
+    name: 'Stephen King',
+    imageUrl: 'images/authors/stepehenking.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['The Shining', 'IT']
+  },
+  {
+    name: 'Charles Dickens',
+    imageUrl: 'images/authors/charlesdickens.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['David Copperfield', 'A Tale Of Two Cities']
+  },
+  {
+    name: 'William Shakespeare',
+    imageUrl: 'images/authors/williamshakespeare.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+  }
+];
+
+function getTurnData(authors) {
+  const allBooks = authors.reduce(fuction (p, c, i) {
+    return p.concat(c.books);
+  }, []);
+  
+}
+
+const state = {
+  turnData: getTurnData(authors)
+};
+
+ReactDOM.render(<AuthorQuiz {...state}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
