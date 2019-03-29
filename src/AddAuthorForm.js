cb6f6df6dffd45b994850f9bc98f533e7c1aee05
egrieshaber <eric.j.grieshaber@gmail.com>
2019-03-29 18:22:47 -0400

    adding functionality to four
    adding event handlers to form, still incomplete at this time.

--- src/AddAuthorForm.js
+++ src/AddAuthorForm.js
@@ -1,16 +1,46 @@
-import React from 'react'
-import './AddAuthorForm.css'
+import React from 'react';
+import './AddAuthorForm.css';
 
-function AddAuthorForm({match}) {
+class AuthorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      imageUrl: ''
+    };
+    this.onFieldChange = this.onFieldChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onAddAuthor(this.state);
+  }
+  onFieldChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+  render() {
+    return <form onSubmit={this.handleSubmit}>
+      <div className="AddAuthorForm_input">
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" value={this.state.name} onChange={this.onFieldChange} />
+      </div>
+      <div className="AddAuthorForm_input">
+        <label htmlFor="imageUrl">Image Url</label>
+        <input type="text" name="imageUrl" value={this.state.imageUrl} onChange={this.onFieldChange} />
+      </div>
+      <input type="submit" value="Add" />
+    </form>;
+  }
+}
+
+function AddAuthorForm({match, onAddAuthor}) {
   return (
   <div classNAme="AddAuthorForm">
     <h1>Add Author</h1>
-      <form>
-        <div className="AddAuthorForm_input">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" />
-        </div>
-      </form>
+    <AuthorForm onAddAuthor={onAddAuthor}/>
   </div>
   );
 }
